d96e241f9bc6fcea011dd8f99b2d42fc0525eb52
aksg0345 <akash0601345@gmail.com>
2024-07-05 10:21:38 +0530

    Update 6_SYSTEM_TASK & COMPLIER_DIRECTIVES.v

--- VERILOG ASSIGNMENT/ASSIGNMENT 4/6_SYSTEM_TASK & COMPLIER_DIRECTIVES.v
+++ VERILOG ASSIGNMENT/ASSIGNMENT 4/6_SYSTEM_TASK & COMPLIER_DIRECTIVES.v
@@ -1,23 +1,7 @@
 1.Write Verilog code in order to generate 5 odd random numbers between 1 to 10.
-  
-  // answer 1
-module is odd (out,in);
-  input[9:0] in;
-  output reg out=0;
-
-  initial begin
-    always@(in)begin
-      if(in%2 == 0);
-      out=0;
-      $display("is even");
-      else
-        out=1;
-      $dispaly("is odd");
-    end
-    endmodule
 
 
-//answer 2
+//answer 1
 module odd_order;
   integer i;
   initial begin
