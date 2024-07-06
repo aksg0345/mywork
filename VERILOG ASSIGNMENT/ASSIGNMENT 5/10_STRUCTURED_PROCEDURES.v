e239ab3974a0428491019665b15b393fd1531200
aksg0345 <akash0601345@gmail.com>
2024-07-06 10:41:45 +0530

    Update 10_STRUCTURED_PROCEDURES.v

--- VERILOG ASSIGNMENT/ASSIGNMENT 5/10_STRUCTURED_PROCEDURES.v
+++ VERILOG ASSIGNMENT/ASSIGNMENT 5/10_STRUCTURED_PROCEDURES.v
@@ -1,1 +1,13 @@
+10.Find the output for the below codes:
 
+1.code:
+reg a, b;
+always @(a, b) begin
+    #10 a <= 1'b0;
+    #1 b <= 1'b1;
+    a <= 1;
+    #5 b <= a;
+    $display($time, " a=%d, b=%d", a, b);
+end
+
+/ ANSWER: 
