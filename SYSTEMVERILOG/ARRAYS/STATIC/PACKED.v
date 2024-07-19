1016d000d6b05edb1f3798a9540743236d3cf5cc
aksg0345 <akash0601345@gmail.com>
2024-07-19 14:28:57 +0530

    Update PACKED.v

--- SYSTEMVERILOG/ARRAYS/STATIC/PACKED.v
+++ SYSTEMVERILOG/ARRAYS/STATIC/PACKED.v
@@ -78,3 +78,42 @@ endmodule
 
 
 
+-> 3. //three dimensional
+
+
+// Code your testbench here
+// or browse Examples
+module  svpacked;
+  
+  logic [2:0][3:0][7:0] vlsi;
+  
+  initial begin
+      
+    foreach(vlsi[i,j])begin
+    vlsi[0]='haefe_dcea;
+    vlsi[1]='h345_9876;
+    vlsi[2]='h6123_7054;
+      
+      $display("vlsi[%0d][%0d]=0x%0h",i,j,vlsi[i][j]);
+      //  $display("vlsi=%p",vlsi);
+      end
+    end 
+ 
+endmodule
+
+
+
+-> OUTPUT:
+
+# KERNEL: vlsi[2][3]=0x61
+# KERNEL: vlsi[2][2]=0x23
+# KERNEL: vlsi[2][1]=0x70
+# KERNEL: vlsi[2][0]=0x54
+# KERNEL: vlsi[1][3]=0x3
+# KERNEL: vlsi[1][2]=0x45
+# KERNEL: vlsi[1][1]=0x98
+# KERNEL: vlsi[1][0]=0x76
+# KERNEL: vlsi[0][3]=0xae
+# KERNEL: vlsi[0][2]=0xfe
+# KERNEL: vlsi[0][1]=0xdc
+# KERNEL: vlsi[0][0]=0xea
