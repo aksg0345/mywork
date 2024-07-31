e0cfaa177c3f0f78c2afc19cce12d89a47c3d105
aksg0345 <akash0601345@gmail.com>
2024-07-31 16:20:49 +0530

    Create FORK AND JOIN_NONE.v

---
+++ SYSTEMVERILOG/PROCESS/FORK AND JOIN_NONE.v
@@ -0,0 +1,36 @@
+module exampleforkjoin;
+  
+  initial begin
+    fork
+      begin
+      $display("the value of time ", $time);
+      #5
+      $display("the vlaue of time", $time);
+      end
+      
+      begin
+       $display("the value of time ", $time);
+      #10
+      $display("the vlaue of time", $time);
+      end
+      
+      begin
+       $display("the value of time ", $time);
+      #40
+      $display("the vlaue of time", $time);
+      end
+    join_none
+    $display("the value of time completed at",$time );
+  end
+    endmodule
+
+
+OUTPUT:
+
+# KERNEL: the value of time completed at                   0
+# KERNEL: the value of time                    0
+# KERNEL: the value of time                    0
+# KERNEL: the value of time                    0
+# KERNEL: the vlaue of time                   5
+# KERNEL: the vlaue of time                  10
+# KERNEL: the vlaue of time                  40
