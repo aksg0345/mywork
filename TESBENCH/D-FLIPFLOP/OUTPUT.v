a4627498646bd3e1f2e6f98c7c0bc7c65262d7a5
aksg0345 <akash0601345@gmail.com>
2024-08-29 15:11:26 +0530

    Create OUTPUT.v

---
+++ TESBENCH/D-FLIPFLOP/OUTPUT.v
@@ -0,0 +1,54 @@
+# Aldec, Inc. Riviera-PRO version 2023.04.112.8911 built for Linux64 on May 12, 2023.
+# HDL, SystemC, and Assertions simulator, debugger, and design environment.
+# (c) 1999-2023 Aldec, Inc. All rights reserved.
+# ELBREAD: Elaboration process.
+# ELBREAD: Elaboration time 0.0 [s].
+# KERNEL: Main thread initiated.
+# KERNEL: Kernel process initialization phase.
+# ELAB2: Elaboration final pass...
+# ELAB2: Create instances ...
+# KERNEL: Time resolution set to 1ns.
+# ELAB2: Create instances complete.
+# SLP: Started
+# SLP: Elaboration phase ...
+# SLP: Elaboration phase ... done : 0.0 [s]
+# SLP: Generation phase ...
+# SLP: Generation phase ... done : 0.0 [s]
+# SLP: Finished : 0.1 [s]
+# SLP: 0 primitives and 4 (80.00%) other processes in SLP
+# SLP: 1 (0.33%) signals in SLP and 11 (3.64%) interface signals
+# ELAB2: Elaboration final pass complete - time: 0.1 [s].
+# KERNEL: SLP loading done - time: 0.0 [s].
+# KERNEL: Warning: You are using the Riviera-PRO EDU Edition. The performance of simulation is reduced.
+# KERNEL: Warning: Contact Aldec for available upgrade options - sales@aldec.com.
+# KERNEL: SLP simulation initialization done - time: 0.0 [s].
+# KERNEL: Kernel process initialization done.
+# Allocation: Simulator allocated 6577 kB (elbread=459 elab2=5957 kernel=160 sdf=0)
+# KERNEL: ASDB file was created in location /home/runner/dataset.asdb
+# KERNEL: t is generated
+# KERNEL: t is recieved
+# KERNEL: t is recieved
+# KERNEL: clk = 0, d = 1, reset = 0, q = 0
+# KERNEL: t is generated
+# KERNEL: t is recieved from DUT
+# KERNEL: t is recieved
+# KERNEL: t is recieved
+# KERNEL: clk = 1, d = 1, reset = 0, q = 1
+# KERNEL: t is generated
+# KERNEL: t is recieved from DUT
+# KERNEL: t is recieved
+# KERNEL: clk = 0, d = 1, reset = 1, q = 0
+# KERNEL: t is generated
+# KERNEL: t is recieved from DUT
+# KERNEL: clk = 1, d = 1, reset = 1, q = 0
+# KERNEL: t is generated
+# KERNEL: t is recieved from DUT
+# KERNEL: clk = 0, d = 1, reset = 1, q = 0
+# KERNEL: t is recieved from DUT
+# KERNEL: Packet recieved in environment
+# KERNEL: envi in test
+# KERNEL: clk = 1, d = 1, reset = 1, q = 0
+# KERNEL: clk = 0, d = 1, reset = 0, q = 0
+# KERNEL: clk = 1, d = 1, reset = 0, q = 1
+# KERNEL: clk = 0, d = 1, reset = 0, q = 1
+# KERNEL: clk = 1, d = 1, reset = 0, q = 1
